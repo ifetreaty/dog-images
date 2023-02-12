842ac1b9f579c7a00cdd79b1ee08a22897a74cc3
Ife Onabanjo <ifeoluwaonabanjo@gmail.com>
2023-02-12 22:32:11 +0100

    feat: create a basic landing page that holds a search bar and a container for images

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,43 @@
 <template>
   <div id="app">
+    <HomePage />
+    {{ searchImages }}
   </div>
 </template>
+
+<script>
+import HomePage from './views/Home-Page.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HomePage,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
